fb808b6df5be982035b534cbad5e59c50b957125
Brandon Fannin <bfannin13@gmail.com>
2020-06-05 12:56:57 -0400

    navbar, pages, and routing

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+<template lang="pug">
+div#app
+  b-navbar#nav(toggleable="sm" type="light" variant="warning")
+    b-navbar-toggle(target="nav-text-collapse")
+    b-navbar-brand.brand brandonfannin.com
+    b-collapse#nav-text-collapse(is-nav)
+      b-navbar-nav
+        b-nav-item(to="/") Home
+      b-navbar-nav
+        b-nav-item(to="/projects") Projects
+      b-navbar-nav
+        b-nav-item(to="/blog") Blog
+      b-navbar-nav
+        b-nav-item(to="/about") Bio
+  router-view
 </template>
 
 <style lang="sass">
@@ -15,12 +22,17 @@
   -moz-osx-font-smoothing: grayscale
   text-align: center
   color: #2c3e50
+  margin: 0px
+  padding: 0px
 
 #nav
-  padding: 30px
+  width: 100%
   a
     font-weight: bold
-    color: #2c3e50
+    color: #f4a261
   a.router-link-exact-active 
-    color: #42b983
+    color: #fff
+  .brand
+    color: #4580c0
+    font-weight: bold
 </style>
