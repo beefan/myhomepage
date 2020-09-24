2ad0c87f20ae99a8d83643c9ead4fd6d4886699d
Brandon Fannin <bfannin13@gmail.com>
2020-09-23 22:06:21 -0400

    node helper to generate directory for markdown blog posts

---
+++ src/assets/blogs/generate-directory.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+
+updateBlogDirectory();
+
+function updateBlogDirectory() {
+    let directory = require('./directory.json');
+
+    newFiles = getNewFiles(directory);
+    newFiles.forEach(filename => {
+        addNewFileToDirectory(directory, filename)
+    });
+
+    fs.writeFileSync('./src/assets/blogs/directory.json', JSON.stringify(directory));
+}
+
+function getNewFiles(directory) {
+    files = getAllMarkdownFiles();
+    return files.filter(x => !directory.files.includes(x));
+}
+
+function getTagsFromFile(filename) {
+    path = './src/assets/blogs/' + filename;
+    file = fs.readFileSync(path, 'utf8');
+
+    indexOfTags = file.indexOf(':tags') + 5;
+    indexOfEndOfTags = file.indexOf('\n', indexOfTags);
+
+    tags = file.substring(indexOfTags, indexOfEndOfTags)
+        .trim()
+        .split(', ');
+
+    return tags;
+}
+
+function getAllMarkdownFiles() {
+    const files = fs.readdirSync('./src/assets/blogs/');
+    return files.filter(file => file.substring(file.length - 3) == ".md");
+}
+
+function addNewFileToDirectory(directory, filename) {
+    directory.files.push(filename);
+    tags = getTagsFromFile(filename);
+    index = directory.files.length - 1;
+    tags.forEach(tag => {
+        directory.tags[tag] ? directory.tags[tag].push(index) : directory.tags[tag] = [index];
+    });
+}
