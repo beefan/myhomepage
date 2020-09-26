100c5970b9481f9971a78450bd35cbf5d6de4608
Brandon Fannin <bfannin13@gmail.com>
2020-09-25 21:50:00 -0400

    cleaned up unused components and dependencies

--- src/main.js
+++ src/main.js
@@ -4,16 +4,6 @@ import router from "./router";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import {
-  faGithub,
-  faLinkedin,
-  faStrava,
-  faTwitter
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import VueTimeline from "@growthbunker/vuetimeline";
 import VueShowdown from 'vue-showdown'
 
 // the second parameter of Vue.use() is optional
@@ -26,13 +16,6 @@ Vue.use(VueShowdown, {
   },
 })
 
-Vue.use(VueTimeline, {
-  // Specify the theme to use: dark or light (dark by default).
-  theme: "light"
-});
-library.add({ faEnvelope, faGithub, faLinkedin, faStrava, faTwitter });
-
-Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 Vue.config.productionTip = false;
