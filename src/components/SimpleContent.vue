694104210acaf22732b96c63984b63f7e592cf8f
Brandon Fannin <bfannin13@gmail.com>
2023-10-09 19:45:15 -0400

    fix content on front page

--- src/components/SimpleContent.vue
+++ src/components/SimpleContent.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
 #content
-  p hi, welcome. <a :href="links.linkedin" >linkedin</a> | <a :href="links.github" >github</a> | <a :href="links.twitter" >twitter</a> | <a :href="links.strava" >strava</a> | <a :href="links.email" >email</a>
+  p hi, welcome. <a :href="links.linkedin" >linkedin</a> | <a :href="links.github" >github</a> | <a :href="links.strava" >strava</a> | <a :href="links.email" >email</a>
   hr
-  p I'm a software developer at Root Insurance in Columbus, Ohio. I have B.A. in Creative Writing. I'm fascinated by soil ecosystems and the contribution of microorganisms to sustainable food systems and carbon sequestration. 🌶️ 🍄 🥦
-  p How can we use machine learning to validate the efficiency and sustainability of new food systems design? How can we use natural language processing to create technology that takes human eccentricities into account? 💻 🧠 🤔
-  p When I'm not thinking, I love getting outside in any way: camping, running, backpacking, biking, hiking. ⛺ 🏃‍♂️ 🚴‍♂️ ⛰️ 🌲
+  p I'm a Laravel and Vue.js developer at Loop Returns, working remotely from Athens, Ohio. 
+  p I have B.A. in English. Lately I've been curious about large language models (like everyone else 😅)
+  p When I'm not thinking, I love getting outside in any way: running, biking, hiking, camping, backpacking ⛺ 🏃‍♂️ 🚴‍♂️ ⛰️ 🌲
   br
   hr
   p some writing <a :href="links.blog">general blog</a> | <a :href="links.te" >coding bootcamp blog</a> | <a :href="links.meditation">vipassana meditation</a>
@@ -20,7 +20,6 @@ export default {
         email: "mailto:dev@brandonfannin.com",
         github: "https://github.com/beefan",
         strava: "https://www.strava.com/athletes/brandonfannin",
-        twitter: "https://twitter.com/btfannin",
         linkedin: "https://linkedin.com/in/brandon-fannin",
         chess: "https://vue-js-chess.netlify.app/",
         avatar: "https://avatarsentiment.netlify.app/",
